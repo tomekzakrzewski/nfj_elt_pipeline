5e718062eeb7d33a06411a3b94324dd91b088a20
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2024-11-03 21:59:50 +0100

    implement scraping json, add .gitignore

--- nfj_scrape.py
+++ nfj_scrape.py
@@ -2,10 +2,11 @@ import requests
 import urllib.parse
 import time
 
-SIZE = 15000
+# HOW SHOULD PROJECT STRUCTURE LOOK, WHERE SHOULD BE INITIAL SCRAPE
+# HOW CAN I USE THIS IN ANOTHER FILE
 BASE_URL = "https://nofluffjobs.com/api/search/posting"
 
-categories = [
+CATEGORIES = [
     "sys-administrator",
     "business-analyst",
     "architecture",
@@ -26,38 +27,60 @@ categories = [
     "other",
 ]
 
-
-headers = {
+HEADERS = {
     "accept": "application/json, text/plain, */*",
     "content-type": "application/infiniteSearch+json",
 }
 
-query_params = {
-    "sort": "newest",
-    "pageTo": 1,
-    "pageSize": SIZE,
-    "salaryCurrency": "PLN",
-    "salaryPeriod": "month",
-    "region": "pl",
-    "language": "en-GB",
-}
 
-url = f"{BASE_URL}?{urllib.parse.urlencode(query_params)}"
+def scrape(pageSize: int):
+    payload = get_payload(pageSize)
+    query_params = get_query_params(pageSize)
+    url = get_url(query_params)
 
-payload = {
-    "criteria": f"category={','.join(categories)}",
-    "url": {"searchParam": "artificial-intelligence"},
-    "rawSearch": f"artificial-intelligence category={','.join(categories)} ",
-    "pageSize": SIZE,
-    "withSalaryMatch": False,
-}
-
-
-def scrape():
     print("started scraping")
     start = time.time()
-    response = requests.post(url, headers=headers, json=payload)
+
+    response = requests.post(url, headers=HEADERS, json=payload)
     response.raise_for_status()
 
+    data = response.json()
 
-# HOW SHOULD PROJECT STRUCTURE LOOK, WHERE SHOULD BE INITIAL SCRAPE
+    unix_timestamp = int(time.time())
+    end = time.time()
+    took = end - start
+    print(f"scaping went successfull")
+    print(f"took {took:.2f}")
+    print(data)
+
+
+def get_url(query_params: dict):
+    url = f"{BASE_URL}?{urllib.parse.urlencode(query_params)}"
+
+    return url
+
+
+def get_payload(pageSize: int) -> dict:
+    payload = {
+        "criteria": f"category={','.join(CATEGORIES)}",
+        "url": {"searchParam": "artificial-intelligence"},
+        "rawSearch": f"artificial-intelligence category={','.join(CATEGORIES)} ",
+        "pageSize": pageSize,
+        "withSalaryMatch": False,
+    }
+
+    return payload
+
+
+def get_query_params(pageSize: int) -> dict:
+    query_params = {
+        "sort": "newest",
+        "pageTo": 1,
+        "pageSize": pageSize,
+        "salaryCurrency": "PLN",
+        "salaryPeriod": "month",
+        "region": "pl",
+        "language": "en-GB",
+    }
+
+    return query_params
