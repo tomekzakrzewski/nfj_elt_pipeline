84b54334c6618bef24a244b6d72d8d6ce7b5710a
tomekzakrzewski <tomekzakrzewskid@gmail.com>
2024-11-14 14:16:07 +0100

    fix dbt transformations, working

--- dbt_transforms/models/marts/fct_job_requirements.sql
+++ dbt_transforms/models/marts/fct_job_requirements.sql
@@ -6,12 +6,12 @@
 }}
 
 SELECT DISTINCT
-    job_id,
-    requirement_id
-FROM {{ source('staging', 'stg_job_requirements') }}
+    new_data.job_id,
+    new_data.requirement_id
+FROM {{ source('staging', 'stg_job_requirements') }} AS new_data
 {% if is_incremental() %}
-WHERE (job_id, requirement_id) NOT IN (
-    SELECT job_id, requirement_id
-    FROM {{ this }}
-)
+LEFT JOIN {{ this }} AS existing_data
+ON new_data.job_id = existing_data.job_id
+AND new_data.requirement_id = existing_data.requirement_id
+WHERE existing_data.job_id IS NULL  -- Only select rows that don't exist in target
 {% endif %}
